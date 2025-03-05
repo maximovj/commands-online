d0dc11330f117619b85dd6849c75775b0d4f31b8
Víctor J. <maximovj@outlook.com>
2025-03-05 15:37:34 -0600

    * feature: Se agrega componente GitHubCorner.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Container from "./components/Container";
+import GitHubCorner from "./components/GitHubCorner";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         </div>
       </Container>
       <Footer />
+      <GitHubCorner />
     </>
   )
 }
