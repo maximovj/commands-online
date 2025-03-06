bd8f4ccae8e381a9eb41298e6ca579cad17bdc4a
Víctor J. <maximovj@outlook.com>
2025-03-06 10:17:04 -0600

    * feature: Se modifica App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ToolBar from "./components/ToolBar";
 import Container from "./components/Container";
 import GitHubCorner from "./components/GitHubCorner";
+import NotFound from "./components/NotFound";
+import ButtonUp from "./components/ButtonUp";
+import CommandList from "./components/CommandList";
+import { useState } from "react";
 
 function App() {
+
+  const [success, setSuccess] = useState(true);
+
+  const showCards = () => {
+    setSuccess(!success);
+  }
+
   return (
     <>
+      <GitHubCorner />
+      <ButtonUp />
       <Header />
       <Container>
-        <div className='h-screen flex flex-col items-center justify-center'>
-          <h1 className='text-center text-red-500 text-bold text-2xl'>
-            Hola mundo desde React + Vite + Tailwind CSS
-          </h1>
+        <div className='min-h-screen flex flex-col items-center justify-center'>
+          <ToolBar showCards={showCards} success={success} />
+          {
+            success ? (
+              <div className="flex-grow w-full h-full m-4">
+                {/* bg-red-400 */}
+                <CommandList title="OOPS" text="Lo siento, elementos no encontrados..." />
+              </div>
+            ) : null
+          }
         </div>
       </Container>
       <Footer />
-      <GitHubCorner />
+
     </>
   )
 }
