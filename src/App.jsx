4d4e855c3b3ac9c15d7fbd2b2eae4a35b337474e
Víctor J. <maximovj@outlook.com>
2025-03-10 21:20:22 -0600

    * feature: Se modifica App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Layout from './components/Layout';
 import NotFound from './pages/NotFound';
 import Home from './pages/Home';
 import All from './pages/All';
+import BackEnd from './pages/BackEnd';
+import DevOps from './pages/DevOps';
+import FrontEnd from './pages/FrontEnd';
+import Tools from './pages/Tools';
 
 const App = () => {
     return (
@@ -11,6 +15,10 @@ const App = () => {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path='all' element={<All />}></Route>
+                    <Route path='backend' element={<BackEnd />}></Route>
+                    <Route path='frontend' element={<FrontEnd />}></Route>
+                    <Route path='devops' element={<DevOps />}></Route>
+                    <Route path='tools' element={<Tools />}></Route>
                     <Route path="*" element={<NotFound title={'OOPS'} text={'Página no encontrada'} />} /> {/* Ruta para manejo de 404 */}
                 </Route>
             </Routes>
