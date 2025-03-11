3143f979fadff21c1efe4bd865fd80916750fc16
Víctor J. <maximovj@outlook.com>
2025-03-10 21:19:40 -0600

    * feature: Se modifica página Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ import ToolBar from "../components/ToolBar";
 
 function Home() {
     return (<>
-        <ToolBar success={false} />
+        <div className="min-h-screen flex flex-col justify-center align-middle items-center">
+            <ToolBar success={false} />
+        </div>
     </>)
 }
 
